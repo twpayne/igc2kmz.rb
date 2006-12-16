1c4bb29229ba1349d1b891f7aba73106866c2eb3
Tom Payne <twpayne@gmail.com>
2006-12-16 20:27:02 +0000

    Add width and height attibutes to photos.

--- lib/photo/kmz.rb
+++ lib/photo/kmz.rb
@@ -7,7 +7,7 @@ class Photo
     point = KML::Point.new(:coordinates => hints.igc.fix_at(time(hints)), :altitudeMode => :absolute)
     name = File.basename(@uri.path)
     src = @uri.scheme ? @uri : "images/#{name}"
-    description = KML::Description.new(KML::CData.new("<img alt=\"#{name}\" src=\"#{src}\" />"))
+    description = KML::Description.new(KML::CData.new("<img alt=\"#{name}\" src=\"#{src}\" width=\"#{@jpeg.width}\" height=\"#{@jpeg.height}\" />"))
     placemark = KML::Placemark.new(point, KML::Name.new(name), description, KML::Snippet.new, KML::StyleUrl.new(hints.stock.photo_style.url), options)
     files = {}
     files["images/#{name}"] = File.open(@uri.to_s) unless @uri.scheme
