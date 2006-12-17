398f0a863f1ed54f18d8b9477ce9af487e31ee1e
Tom Payne <twpayne@gmail.com>
2006-12-17 10:09:31 +0000

    Label XC legs with distance. Fixes #21.

--- lib/optima/kmz.rb
+++ lib/optima/kmz.rb
@@ -4,7 +4,7 @@ require "optima"
 class Optimum
 
   def to_kmz(hints, folder_options = {})
-    name = (@multiplier.zero? ? '%s (%.1fkm)' : '%s (%.1fkm, %.2f points)') % [@flight_type, distance / 1000.0, score]
+    name = (@multiplier.zero? ? "%s (%.1fkm)" : "%s (%.1fkm, %.2f points)") % [@flight_type, distance / 1000.0, score]
     folder = KML::Folder.hide_children(KML::Name.new(name), folder_options)
     if @circuit
       coordinates = @fixes[1...-1]
@@ -12,9 +12,14 @@ class Optimum
     else
       coordinates = @fixes
     end
-    line_string = KML::LineString.new(:coordinates => coordinates, :tessellate => 1)
-    placemark = KML::Placemark.new(line_string, :styleUrl => hints.stock.distance_style.url)
-    folder.add(placemark)
+    coordinates.each_cons(2) do |coord0, coord1|
+      line_string = KML::LineString.new(:coordinates => [coord0, coord1], :tessellate => 1)
+      point = KML::Point.new(:coordinates => coord0.halfway_to(coord1))
+      multi_geometry = KML::MultiGeometry.new(line_string, point)
+      name = "%.1fkm" % (coord0.distance_to(coord1) / 1000)
+      placemark = KML::Placemark.new(multi_geometry, :name => name, :styleUrl => hints.stock.distance_style.url)
+      folder.add(placemark)
+    end
     @fixes.each_with_index do |fix, index|
       folder.add(fix.to_kml(hints, @names[index], nil, :styleUrl => hints.stock.distance_style.url))
     end
