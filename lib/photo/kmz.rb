480459cbc5d23f825cbb5d3f19411317cb00b076
Tom Payne <twpayne@gmail.com>
2006-12-26 16:10:14 +0000

    Store photos in a separate directory.

--- lib/photo/kmz.rb
+++ lib/photo/kmz.rb
@@ -6,7 +6,7 @@ class Photo
   def to_kmz(hints, options = {})
     point = KML::Point.new(:coordinates => hints.igc.fix_at(@time + hints.photo_tz_offset - hints.tz_offset), :altitudeMode => :absolute)
     name = File.basename(@uri.path)
-    src = @uri.scheme ? @uri : "images/#{name}"
+    src = @uri.scheme ? @uri : "images/photos/#{name}"
     description = KML::Description.new(KML::CData.new("<img alt=\"#{name}\" src=\"#{src}\" width=\"#{@jpeg.width}\" height=\"#{@jpeg.height}\" />"))
     placemark = KML::Placemark.new(point, KML::Name.new(name), description, KML::Snippet.new, options)
     files = {}
