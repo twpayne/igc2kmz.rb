58c71b16a2586f0b0836a3bf82bb8edf94de0f59
Tom Payne <twpayne@gmail.com>
2007-01-28 21:51:32 +0000

    Fix use of to_xml.

--- lib/photo/kmz.rb
+++ lib/photo/kmz.rb
@@ -6,9 +6,9 @@ class Photo
   def to_kmz(hints, options = {})
     point = KML::Point.new(:coordinates => hints.igc.fix_at(@time + hints.photo_tz_offset - hints.tz_offset), :altitudeMode => hints.altitude_mode)
     name = File.basename(@uri.path)
-    src = @uri.scheme ? @uri : "images/photos/#{name}"
+    src = @uri.scheme ? @uri.to_s : "images/photos/#{name}"
     description = KML::Description.new(KML::CData.new("<img alt=\"#{name.to_xml}\" src=\"#{src.to_xml}\" width=\"#{@jpeg.width}\" height=\"#{@jpeg.height}\" />"))
-    placemark = KML::Placemark.new(point, KML::Name.new(name.to_kml), description, KML::Snippet.new, options)
+    placemark = KML::Placemark.new(point, KML::Name.new(name.to_xml), description, KML::Snippet.new, options)
     files = {}
     files["images/#{name}"] = File.open(@uri.to_s) unless @uri.scheme
     KMZ.new(placemark, :files => files)
