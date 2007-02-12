a7b9588ef5066b4fd02abea6ee0b263777e46cd7
Tom Payne <twpayne@gmail.com>
2007-02-12 16:19:04 +0000

    Move Magick functions into separate file.

--- lib/igc/kmz.rb
+++ lib/igc/kmz.rb
@@ -6,6 +6,7 @@ require "kml/rmagick"
 require "kmz"
 require "ostruct"
 require "photo/kmz"
+require "magick"
 require "sponsor/kmz"
 require "task"
 require "task/kmz"
@@ -13,56 +14,6 @@ require "units"
 require "rvg/rvg"
 require "xc"
 require "xc/kmz"
-
-module Gradient
-
-  class Grayscale
-
-    class << self
-
-      def [](delta)
-        value = delta * Magick::MaxRGB
-        Magick::Pixel.new(value, value, value)
-      end
-
-    end
-
-  end
-
-  class Default
-
-    class << self
-
-      def [](delta)
-        hue = 2.0 * (1.0 - delta.constrain(0.0, 1.0)) / 3.0
-        Magick::Pixel.from_HSL([hue, 1.0, 0.5])
-      end
-
-    end
-
-  end
-
-end
-
-module Magick
-
-  class Image
-
-    def outline(&block)
-      mask = black_threshold(Magick::MaxRGB + 1)
-      image = Image.new(columns, rows, &block)
-      (-1..1).each do |i|
-        (-1..1).each do |j|
-          image.composite!(mask, i, j, Magick::MultiplyCompositeOp)
-        end
-      end
-      image.composite!(self, 0, 0, Magick::OverCompositeOp)
-      image
-    end
-
-  end
-
-end
 
 class Scale
 
