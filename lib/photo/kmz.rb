a42330772a232e4fcd10ef180ca10e64d6a024ff
Tom Payne <twpayne@gmail.com>
2007-03-21 23:59:16 +0000

    Use new, friendlier KML API.

--- lib/photo/kmz.rb
+++ lib/photo/kmz.rb
@@ -5,7 +5,9 @@ require "kmz"
 class Photo
 
   def to_kmz(hints, options = {})
-    point = KML::Point.new(:coordinates => hints.igc.fix_at(@time + hints.photo_tz_offset - hints.tz_offset), :altitudeMode => hints.altitude_mode)
+    point = KML::Point.new
+    point.coordinates = hints.igc.fix_at(@time + hints.photo_tz_offset - hints.tz_offset)
+    point.altitude_mode = hints.altitude_mode
     name = File.basename(@uri.is_a?(URI) ? @uri.path : @uri)
     src = @uri.is_a?(URI) ? @uri.to_s : "images/photos/#{name}"
     files = {}
@@ -23,8 +25,10 @@ class Photo
     else
       files[src] = File.open(@uri.to_s) unless @uri.is_a?(URI)
     end
-    description = KML::Description.new(KML::CData.new("<img alt=\"#{name.to_xml}\" src=\"#{src.to_xml}\" width=\"#{width}\" height=\"#{height}\" />"))
-    placemark = KML::Placemark.new(point, KML::Name.new(name.sub(/\.jpe?g/i, "").to_xml), description, KML::Snippet.new, options)
+    placemark = KML::Placemark.new(point, options)
+    placemark.name = name.sub(/\.jpe?g/i, "").to_xml
+    placemark.description = KML::CData.new("<img alt=\"#{name.to_xml}\" src=\"#{src.to_xml}\" width=\"#{width}\" height=\"#{height}\" />")
+    placemark.snippet = nil
     KMZ.new(placemark, :files => files)
   end
 
