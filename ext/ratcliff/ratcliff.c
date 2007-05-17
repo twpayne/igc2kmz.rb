fa8b370b35aa07482285f04cbf3bdd736cc7d100
Tom Payne <twpayne@gmail.com>
2007-05-17 17:06:49 +0000

    Cleanups.

--- ext/ratcliff/ratcliff.c
+++ ext/ratcliff/ratcliff.c
@@ -14,11 +14,11 @@ ratcliff_obershelp(const char *begin1, const char *end1, const char *begin2, con
 	const char *limit2 = end2;
 	for (i1 = begin1; i1 < limit1; ++i1) {
 		for (i2 = begin2; i2 < limit2; ++i2) {
-			if (*i1 == *i2){
+			if (*i1 == *i2) {
 				int j = 1;
 				while (i1 + j < end1 && i2 + j < end2 && i1[j] == i2[j])
 					++j;
-				if (j > length){
+				if (j > length) {
 					length = j;
 					start1 = i1;
 					start2 = i2;
@@ -28,22 +28,17 @@ ratcliff_obershelp(const char *begin1, const char *end1, const char *begin2, con
 			}
         }
     }
-	if (!length)
-		return 0;
-	return ratcliff_obershelp(begin1, start1, begin2, start2) + length + ratcliff_obershelp(start1 + length, end1, start2 + length, end2);
-
+	return length ? ratcliff_obershelp(begin1, start1, begin2, start2) + length + ratcliff_obershelp(start1 + length, end1, start2 + length, end2) : 0;
 }
 
 static VALUE
 String_ratcliff(VALUE self, VALUE s)
 {
-	double result;
 	Check_Type(s, T_STRING);
-	if(RSTRING(self)->len == 1 && RSTRING(s)->len == 1)
-		result = RSTRING(self)->ptr[0] == RSTRING(s)->ptr[0] ? 1.0 : 0.0;
+	if (RSTRING(self)->len == 1 && RSTRING(s)->len == 1)
+		return INT2FIX(RSTRING(self)->ptr[0] == RSTRING(s)->ptr[0] ? 1 : 0);
 	else
-		result = 2.0 * ratcliff_obershelp(RSTRING(self)->ptr, RSTRING(self)->ptr+RSTRING(self)->len, RSTRING(s)->ptr, RSTRING(s)->ptr+RSTRING(s)->len) / (RSTRING(self)->len + RSTRING(s)->len);
-	return rb_float_new(result);
+		return rb_float_new(2.0 * ratcliff_obershelp(RSTRING(self)->ptr, RSTRING(self)->ptr + RSTRING(self)->len, RSTRING(s)->ptr, RSTRING(s)->ptr + RSTRING(s)->len) / (RSTRING(self)->len + RSTRING(s)->len));
 
 }
 
