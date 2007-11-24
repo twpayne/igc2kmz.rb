3536dd7ee4b527d6d8d561d0aeb0a5dbe33fe064
Tom Payne <twpayne@gmail.com>
2007-11-24 18:45:00 +0000

    Add string functions.

--- lib/html.rb
+++ lib/html.rb
@@ -17,3 +17,43 @@ class Array
   end
 
 end
+
+class String
+
+  def html_entity_encode
+    gsub(/&|>|<|"/) do |match|
+      case match
+      when '&' then '&amp;'
+      when '<' then '&gt;'
+      when '>' then '&lt;'
+      when '"' then '&quot;'
+      end
+    end
+  end
+
+  def html_entity_decode
+    gsub(/&([^;]*);/) do |match|
+      case match
+      when '&amp;'  then '&'
+      when '&gt;'   then '>'
+      when '&lt;'   then '<'
+      when '&quot;' then '"'
+      when /&#([0-9A-Fa-f]+);/ then Regexp.last_match[1].hex.chr
+      else match
+      end
+    end
+  end
+
+  def url_encode
+    gsub(/[^0-9A-Za-z]/) do |match|
+      match == ' ' ? '+' : sprintf('%%%02x', match[0])
+    end
+  end 
+
+  def url_decode
+    gsub(/\+|%[0-9A-Fa-f][0-9A-Fa-f]/) do |match|
+      match == '+' ? ' ' : match[1..2].hex.chr
+    end
+  end
+
+end
